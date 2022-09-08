2b192bffa84c80b087276997af3e92d86a84e659
Jeff <odhiambo47@gmail.com>
2022-09-08 23:11:28 +0300

    added an event listener

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
+//Event listener
+btn.addEventListener("click", () => {
+    let inpWord = document.getElementById("inp-word").value;
