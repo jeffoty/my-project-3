d2531dc218378c9b14fb9c1b739567b68cc88df1
jeffoty <107908318+jeffoty@users.noreply.github.com>
2022-09-11 02:00:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ btn.addEventListener("click", () => {
         .then((data) => {
             console.log(data);
             result.innerHTML = `
-            <div class="word">
-                    <h3>${inpWord}</h3>
-                    <button onclick="playSound()">
-                        <i class="fas fa-volume-up"></i>
-                    </button>
-                </div>
                 <div class="details">
                     <p>${data[0].meanings[0].partOfSpeech}</p>
                     <p>/${data[0].phonetic}/</p>
